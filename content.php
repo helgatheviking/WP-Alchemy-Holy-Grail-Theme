ac3c198aaaae4d59aad4654e903370022644bfc0
Kathy Darling <helgatheviking@gmail.com>
2014-04-01 18:11:48 +0200

    Switch to Twenty Fourteen Child theme

---
+++ content.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php twentyfourteen_post_thumbnail(); ?>
+
+	<header class="entry-header">
+		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
+		<div class="entry-meta">
+			<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
+		</div>
+		<?php
+			endif;
+
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+			endif;
+		?>
+
+		<div class="entry-meta">
+			<?php
+				if ( 'post' == get_post_type() )
+					twentyfourteen_posted_on();
+
+				if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
+			?>
+			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyfourteen' ), __( '1 Comment', 'twentyfourteen' ), __( '% Comments', 'twentyfourteen' ) ); ?></span>
+			<?php
+				endif;
+
+				edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' );
+			?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<?php if ( is_search() ) : ?>
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-summary -->
+	<?php else : ?>
+	<div class="entry-content">
+		<?php
+			the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ) );
+
+			// Display the alchemy text boxes
+			global $simple_textarea, $repeating_textareas; ?>
+
+			<div id="alchemy-results">
+
+			<?php
+
+			if( isset( $simple_textarea ) ){
+				echo '<h2>' . __( 'Simple Text Area', 'wpalchemy-grail' ) . '</h2>';
+				$simple_textarea->the_meta(); 
+				echo apply_filters( 'meta_content', $simple_textarea->get_the_value( 'test_editor' ) );
+			}
+			
+			if( isset( $repeating_textareas ) ){
+				echo '<h2>' . __( 'Repeating Text Areas', 'wpalchemy-grail' ) . '</h2>';
+
+				$repeating_textareas->the_meta();
+				while( $repeating_textareas->have_fields( 'repeating_textareas' ) ){
+					echo '<h3>' . sprintf( __( 'Text Area #%d', 'wpalchemy-grail' ), $repeating_textareas->get_the_index() + 1 ) . '</h3>';
+					echo apply_filters( 'meta_content', $repeating_textareas->get_the_value('textarea' ) );
+				} 
+			}
+
+			?>
+			</div><!-- alchemy -->
+
+			<?php
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+	<?php endif; ?>
+
+	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+</article><!-- #post-## -->
