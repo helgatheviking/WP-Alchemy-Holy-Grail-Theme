b25ca315e161f33d3076072322f7eb98e7dec977
Kathy Darling <helgatheviking@gmail.com>
2014-05-01 14:58:55 +0200

    update scripts for WordPress 3.9

--- metaboxes/kia-metabox.js
+++ metaboxes/kia-metabox.js
@@ -2,8 +2,7 @@
 /* KIA Metabox scripts
 /*
 /* upload media buttons, sort, repeatable tinyMCE fields 
-/* requires jquery 1.7
-/* tested on WordPress 3.3.1
+/* requires WordPress 3.9
 /*
 /* © Kathy Darling http://www.kathyisawesome.com
 /* 2012-03-07. */
@@ -12,130 +11,159 @@
 
 ;(function ($) {
 
-	var KIA_metabox = {
- 
-/*-----------------------------------------------------------------------------------*/
-/* Launch tinyMCE, handle tinyMCE on repeating fields
-/*-----------------------------------------------------------------------------------*/
-	tinyMCE: function() { 
-		
-		//run our tinyMCE script on textareas at load
-		KIA_metabox.runTinyMCE();
-		
-		//create a div to bind to
-		if ( ! $.wpalchemy ) {
-			$.wpalchemy = $('<div/>').attr('id','wpalchemy').appendTo('body');
-		};
-		
-		//run our tinyMCE script on textareas when copy is made
-		$(document.body).on('wpa_copy', $.wpalchemy, function() { 		
-			KIA_metabox.runTinyMCE();
-		});
+var KIA_metabox = {
+
+	/*-----------------------------------------------------------------------------------*/
+	/* All the matching text areas 
+	/*-----------------------------------------------------------------------------------*/
+
+	textareas: {},
+
+	/*-----------------------------------------------------------------------------------*/
+	/* tinyMCE settings
+	/*-----------------------------------------------------------------------------------*/
+
+	tmc_settings: {},
+
+	/*-----------------------------------------------------------------------------------*/
+	/* tinyMCE defaults
+	/*-----------------------------------------------------------------------------------*/
+
+	tmc_defaults: {
+		theme: 'modern',
+		menubar: false,
+		wpautop: true,
+		indent: false,
+		toolbar1: 'bold,italic,underline,blockquote,strikethrough,bullist,numlist,alignleft,aligncenter,alignright,undo,redo,link,unlink,fullscreen',
+		plugins: 'fullscreen,image,wordpress,wpeditimage,wplink'
 	},
-	
-/*-----------------------------------------------------------------------------------*/
-/* Repeatable TinyMCE-enhanced textareas
-/*-----------------------------------------------------------------------------------*/
+
+	/*-----------------------------------------------------------------------------------*/
+	/* quicktags settings
+	/*-----------------------------------------------------------------------------------*/
+
+	qt_settings: {},
+
+	/*-----------------------------------------------------------------------------------*/
+	/* quicktags defaults
+	/*-----------------------------------------------------------------------------------*/
+
+	qt_defaults: {
+		buttons: 'strong,em,link,block,del,ins,img,ul,ol,li,code,more,close,fullscreen'
+	},
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Launch TinyMCE-enhanced textareas
+	/*-----------------------------------------------------------------------------------*/
 
 	runTinyMCE: function() {
-	
-			// some settings for a more minimal tinyMCE editor
-			tinyMCEminConfig = 	{ 
-				theme : "advanced",
-				skin:"wp_theme", 
-				mode : "none",
-				language : "en",
-				theme_advanced_resizing:"1", 
-				width:"100%",
-				height : "150",
-				theme_advanced_layout_manager : "SimpleLayout",
-				theme_advanced_toolbar_location : "top",
-				theme_advanced_toolbar_align : "left",
-				theme_advanced_buttons1:"styleselect,formatselect,bold,italic,strikethrough,underline,|,link,unlink,|,forecolor,|undo,redo,|,code",
-				theme_advanced_buttons2 : "",
-				theme_advanced_buttons3 : "",
-				theme_advanced_statusbar_location:"",
-				plugins:"safari,inlinepopups,spellchecker,paste,wordpress,tabfocus",
-				width : "540"
-			};
-			
-			//store the default settings
-			try { 
-				tinyMCEdefaultConfig =  $.extend(true, {}, tinyMCE.settings);
 
-				//tweak the setting just a litte to set the height and to add an HTML code button (since toggling editors is crazy difficult)
-				tinyMCEdefaultConfig.height = "150";
-				tinyMCEdefaultConfig.theme_advanced_buttons1 = tinyMCEdefaultConfig.theme_advanced_buttons1 + ',|,code';
+		// get the #content's tinyMCE settings or use default
+		var init_settings = typeof tinyMCEPreInit == 'object' && 'mceInit' in tinyMCEPreInit && 'content' in tinyMCEPreInit.mceInit ? tinyMCEPreInit.mceInit.content : KIA_metabox.tmc_defaults;
 
-			} catch(e) { 
-				tinyMCEdefaultConfig = tinyMCEminConfig;
+		// get the #content's quicktags settings or use default
+		KIA_metabox.qt_settings = typeof tinyMCEPreInit == 'object' && 'qtInit' in tinyMCEPreInit && 'content' in tinyMCEPreInit.qtInit ? tinyMCEPreInit.qtInit.content : KIA_metabox.qt_defaults;
+
+		var custom_settings = {
+			setup : function(ed) {
+				ed.on('change', function(e) {
+					KIA_metabox.changeName( ed );
+				}); 
 			}
-			
-			//all custom text areas, except the one to copy
-			textareas = $('div.wpa_group:not(.tocopy) .customEditor textarea'); 
-			
-			//count the number of textareas
-			var count = textareas.length;
-			
-			//give each a unique ID so we can apply TinyMCE to each 
-			textareas.each(function(i) {  
-				var id = $(this).attr('id');
-				if (!id) {
-					id = 'customEditor-' + ( i + count ); //doesn't quite count in order, but does always get a unique id#
-					$(this).attr('id', id);
-				}  
-											
-				try { 
+		}
 
-					//if the customEditor div has the minimal class, serve up the minimal tinyMCE configuration
-					if($(this).parent().hasClass('minimal')){
-						tinyMCE.settings = tinyMCEminConfig;	
-					} else {
-						tinyMCE.settings = tinyMCEdefaultConfig;
-					}
+		// merge our settings with WordPress' and store for later use
+		KIA_metabox.tmc_settings = $.extend( {}, init_settings, custom_settings );
 
-					tinyMCE.execCommand('mceAddControl', false, id); 
-				} catch(e){}
-			
-			});  //end each
+		//all custom text areas, except the one to copy
+		KIA_metabox.textareas = $('div.wpa_group:not(.tocopy) textarea.wp-editor-area'); 
 		
+		//give each a unique ID, TinyMCE will need it later
+		KIA_metabox.textareas.each( function( i ) {  
+			var id = $(this).attr( 'id' );
+			if ( !id) {
+				id = 'mceEditor-' + ( i );
+				$( this ).attr( 'id', id );
+			}  
+			
+			// for some reason in WP I am required to do this in the loop 
+			// KIA_metabox.tmc_settings.selector is insufficient, anyone who can tell my why gets a margarita
+			var tmc_settings = $.extend( {}, KIA_metabox.tmc_settings, { selector : "#" + id } );
+
+			var qt_settings = $.extend( {}, KIA_metabox.qt_settings, { id : id } );
+
+			// add our copy to he collection in the tinyMCEPreInit object because switch editors
+			// will look there for an wpautop setting specific to this editor
+			// similarly quicktags will product a toolbar with no buttons: https://core.trac.wordpress.org/ticket/26183
+			if ( typeof tinyMCEPreInit == 'object' ){
+				tinyMCEPreInit.mceInit[id] = tmc_settings;
+				tinyMCEPreInit.qtInit[id] = qt_settings;
+				//switchEditors.addInstance( id, tmc_settings );
+				//QTags.addInstance( qt_settings );
+			}
+
+			// turn on the quicktags editor for each
+			quicktags( qt_settings );
+
+			// turn on tinyMCE for each
+			tinymce.init( tmc_settings );
+
+			// fix media buttons
+			$(this).closest('.customEditor').find('a.insert-media').data( 'editor', id );
+
+		});  //end each	
+
+	} , //end runTinyMCE text areas 
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Apply TinyMCE to new textareas
+	/*-----------------------------------------------------------------------------------*/
+
+	newTinyMCE: function( clone ) { 
+
+		// count all custom text areas, except the one to copy
+		count = KIA_metabox.textareas.length;
+			
+		// assign the new textarea an ID
+		id = 'mceEditor-' + count;
+		$new_textarea = clone.find( 'textarea.wp-editor-area' ).attr( 'id', id );
+
+		// add new textarea to collection
+		KIA_metabox.textareas.push( $new_textarea );
 		 
+		// Merge new selector into settings
+		var tmc_settings = $.extend( {}, KIA_metabox.tmc_settings, { selector : "#" + id } );
+
+		var qt_settings = $.extend( {}, KIA_metabox.qt_settings, { id : id } );
+
+
+		// add our copy to he collection in the tinyMCEPreInit object because switch editors
+		if ( typeof tinyMCEPreInit == 'object' ){
+				tinyMCEPreInit.mceInit[id] = tmc_settings;
+				tinyMCEPreInit.qtInit[id] = qt_settings;
+		}
+
+		//QTags.addInstance( qt_settings );
+
+		try { 
+			// turn on the quicktags editor for each
+			quicktags( qt_settings );
+
+			// old way to initialize - doesn't get our new settings
+			// tinyMCE.execCommand( 'mceAddEditor', false, id ); 
+			
+			// turn on tinyMCE
+			tinyMCE.init( tmc_settings );
+
+		} catch(e){}
+			
 		
 	} , //end runTinyMCE text areas 
-	
-/*-----------------------------------------------------------------------------------*/
-/* Custom Media Upload Buttons for tinyMCE textareas
-/*-----------------------------------------------------------------------------------*/
-
-	mediaButtons: function() {
-	
-			$('body').on('click','.custom_upload_buttons a',function(){  
-				textarea = $(this).closest('.customEditor').find('textarea');   
-				mceID = textarea.attr('id');  
-				kia_backup = window.send_to_editor; // backup the original 'send_to_editor' function
-				window.send_to_editor = window.send_to_editor_clone;
-			});
 			
-			//borrow the send to editor function
-			window.send_to_editor_clone = function(html){
 
-				try {
-					tinyMCE.execInstanceCommand(mceID, 'mceInsertContent', false, html);
-				} catch(e) {
-						$(textarea).insertAtCaret(html); 
-				}
-
-				tb_remove();
-							
-				// restore the default behavior
-				window.send_to_editor = kia_backup;
-			}
-			
-	}, //end mediaButtons			
-
-/*-----------------------------------------------------------------------------------*/
-/* Meta Fields Sorting
-/*-----------------------------------------------------------------------------------*/
+	/*-----------------------------------------------------------------------------------*/
+	/* Meta Fields Sorting
+	/*-----------------------------------------------------------------------------------*/
 	
 	sortable: function(){
 
@@ -147,61 +175,108 @@
 			opacity: 0.5,
 			tolerance: 'pointer',
 			start: function(event, ui) { // turn TinyMCE off while sorting (if not, it won't work when resorted)
-				textareaID = $(ui.item).find('.customEditor textarea').attr('id');
-				try { tinyMCE.execCommand('mceRemoveControl', false, textareaID); } catch(e){}
+				textareaID = $(ui.item).find('textarea.wp-editor-area').attr('id');
+				try { tinyMCE.execCommand('mceRemoveEditor', false, textareaID); } catch(e){}
 			},
 			stop: function(event, ui) { // re-initialize TinyMCE when sort is completed
-				try { tinyMCE.execCommand('mceAddControl', false, textareaID); } catch(e){}
-				$(this).find('.update-warning').show();
+				try { tinyMCE.execCommand('mceAddEditor', false, textareaID); } catch(e){}
+	//			$(this).find('.update-warning').show();
 			}
 		});
 		
 	}, //end of sortable
-	
-	}; // End KIA_metabox Object // Don't remove this, or the sky will fall on your head.
 
- /*-----------------------------------------------------------------------------------*/
+	/*-----------------------------------------------------------------------------------*/
+	/* A Simple Toggle switch 
+	/*-----------------------------------------------------------------------------------*/
+
+	toggleGroups : function(){
+
+		$( '.wpa_loop' ).on( 'click', '.toggle', function() { 
+
+			$group = $(this).parents('.wpa_group'); console.log($group);
+			$toggle = $group.find('.toggle_state'); console.log($toggle);
+			$inside = $group.find('.group-inside'); console.log($inside);
+			
+			$inside.toggle( 'slow', function() {
+			    $toggle.prop( 'checked', ! $toggle.prop( 'checked' ) );
+			    $group.find( '.group-wrap' ).toggleClass( 'closed', $toggle.prop( 'checked' ) );
+			});
+
+		});
+
+	}, //end toggleGroups
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Change Group Name via TinyMCE callback
+	/*-----------------------------------------------------------------------------------*/
+
+	changeName : function( ed ){
+
+		$('#' + ed.id ).closest('.wpa_group').find('.handle').html( ed.getContent({format : 'text'}).substring(0,30)  );
+
+	}, //end changeName
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Group Name
+	/*-----------------------------------------------------------------------------------*/
+
+	groupName : function( ed ){
+
+		$( '.wpa_loop' ).on( 'change', 'textarea.wp-editor-area', function() {
+
+			$group = $(this).parents('.wpa_group');
+
+			$group.find('.handle').html( $(this).val().substring(0,30)  );
+
+		});
+
+	}, //end groupName
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Switch Editors
+	/*-----------------------------------------------------------------------------------*/
+
+	switchEditors : function(){
+
+		$( '.wpa_loop' ).on( 'click', '.wp-switch-editor', function() { 
+
+			$wrapper = $(this).closest('.wp-editor-wrap');
+			$wrapper.toggleClass('html-active tmce-active');
+
+			id = $wrapper.find('textarea.wp-editor-area').attr('id');
+			mode = $(this).data('mode');
+
+			switchEditors.go(id, mode);
+
+		});
+
+	} //end switchEditors
+
+}; // End KIA_metabox Object // Don't remove this, or there's no guacamole for you
+
+/*-----------------------------------------------------------------------------------*/
 /* Execute the above methods in the KIA_metabox object.
 /*-----------------------------------------------------------------------------------*/
   
 	$(document).ready(function () {
 		
-		KIA_metabox.tinyMCE();
-		KIA_metabox.mediaButtons();
+		KIA_metabox.runTinyMCE();
 		KIA_metabox.sortable();
+		KIA_metabox.toggleGroups();
+		KIA_metabox.groupName();
+		KIA_metabox.switchEditors();
+
+		//create a div to bind to
+		if ( ! $.wpalchemy ) {
+			$.wpalchemy = $('<div/>').attr('id','wpalchemy').appendTo('body');
+		};
+		
+		// run our tinyMCE script on textareas when copy is made
+		$( document.body ).on( 'wpa_copy', $.wpalchemy, function( event, clone ) { 		
+			KIA_metabox.newTinyMCE( clone );
+		});
 			
 	});
   
 })(jQuery);
-
-
-
-// jQuery insertAtCaret plugin
-// http://stackoverflow.com/questions/946534/insert-text-into-textarea-with-jquery#answer-2819568
-jQuery.fn.extend({
-insertAtCaret: function(myValue){
-  return this.each(function(i) {
-    if (document.selection) {
-      //For browsers like Internet Explorer
-      this.focus();
-      sel = document.selection.createRange();
-      sel.text = myValue;
-      this.focus();
-    }
-    else if (this.selectionStart || this.selectionStart == '0') {
-      //For browsers like Firefox and Webkit based
-      var startPos = this.selectionStart;
-      var endPos = this.selectionEnd;
-      var scrollTop = this.scrollTop;
-      this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
-      this.focus();
-      this.selectionStart = startPos + myValue.length;
-      this.selectionEnd = startPos + myValue.length;
-      this.scrollTop = scrollTop;
-    } else {
-      this.value += myValue;
-      this.focus();
-    }
-  })
-}
-});
