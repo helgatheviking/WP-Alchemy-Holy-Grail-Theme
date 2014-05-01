b5741aaeac30fb38d834a2edcc1d9b773da0e62e
Kathy Darling <helgatheviking@gmail.com>
2014-05-01 14:38:14 +0200

    html_entitity_decode() on editor $content because WP Alchemy uses htmlentitities() when displaying the value

--- metaboxes/single-textarea.php
+++ metaboxes/single-textarea.php
@@ -3,23 +3,23 @@
 	<p><?php _e('A single textarea that uses the wp_editor() function.');?></p>
  
 	<p>
-		<?php $mb->the_field('test_editor'); 
 
-		$settings = array( 
+		<?php $mb->the_field('test_editor');
+
+		$settings = array(
 			'textarea_rows' => '10',
-			'media_buttons' => 'false',
-			'tabindex' =>2
+			'media_buttons' => true,
+			'tabindex' =>2,
+			'textarea_name' => $mb->get_the_name(),
 		);
 
-		$val =  html_entity_decode( $mb->get_the_value(), ENT_QUOTES, 'UTF-8' ); 
-		$id = $mb->get_the_name();
-		
-		wp_editor($val,  $id , $settings );
+		// need to html_entity_decode() the value b/c WP Alchemy's get_the_value() runs the data through htmlentities()
+		wp_editor( html_entity_decode( $mb->get_the_value() ),  $mb->get_the_id() , $settings );
 		?>
-		
+
 		 <span>Enter in some text</span>
 	</p>
-	
+
 	<p class="meta-save"><button type="submit" class="button-primary" name="save"><?php _e('Update');?></button></p>
 
 </div>
